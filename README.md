3d212f54f9a2a85327ca18343700096875cb1076
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-06-14 09:47:22 +0200

    feat: 🎸 ça marche

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # IOT MQTT
 
 [Matthys Ducrocq](https://github.com/matthysdev)
-
 [Corentyn Masset](https://github.com/corentyn)
+
+## Documentation
+
+### Requirements
+
+- Node.js
+
+### Installation
+
+```bash
+npm install
+```
+
+### Usage
+
+```bash
+node index.js
+```
--- requeue.js
+++ requeue.js
@@ -3,20 +3,21 @@ const mongoose = require('mongoose');
 const Message = require('./models/message');
 
 // connexion à la base de données MongoDB
-mongoose.connect('mongodb://localhost:27017/mqtt_queue');
+mongoose.connect('mongodb+srv://MatthysDev:passwordtooweak@cluster0.gl8aj0c.mongodb.net/');
 
 // fonction pour requeuer les messages en attente depuis trop longtemps
 async function requeueMessages() {
     while (true) {
+        console.log('Checking for expired messages...');
         const currentTime = new Date();
-        const timeout = 30000; // 30 secondes
+        const timeout = 1000; // 30 secondes
         // récupérer les messages en cours depuis trop longtemps
         const expiredMessages = await Message.find({ status: 'in-progress', timestamp: { $lt: new Date(currentTime - timeout) } });
 
         // requeuer les messages
         for (const message of expiredMessages) {
-            await Message.findByIdAndUpdate(message._id, { status: 'pending', $inc: { attempts: 1 } });
             console.log(`Message ${message._id} requeued.`);
+            await Message.findByIdAndUpdate(message._id, { status: 'pending', $inc: { attempts: 1 } });
         }
         // attendre 10 secondes avant de vérifier à nouveau
         await new Promise(resolve => setTimeout(resolve, 10000));
